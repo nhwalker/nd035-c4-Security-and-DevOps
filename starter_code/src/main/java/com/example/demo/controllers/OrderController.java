e46a150397e9b740390f9e6444121e5a138e924b
N.H. Walker <42497130+nhwalker@users.noreply.github.com>
2023-09-29 16:42:46 -0400

    Add Logging
    Add logging

--- starter_code/src/main/java/com/example/demo/controllers/OrderController.java
+++ starter_code/src/main/java/com/example/demo/controllers/OrderController.java
@@ -2,6 +2,10 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,23 +24,34 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 @RestController
 @RequestMapping("/api/order")
 public class OrderController {
-	
+	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 	
 	@Autowired
 	private UserRepository userRepository;
 	
 	@Autowired
 	private OrderRepository orderRepository;
-	
+	@Autowired
+	private ObjectMapper json;
 	
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		log.debug("Submitting Order username:{}", username);
+
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.warn("Failed to submit order. User not found. username:{}", username);
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+
+		try {
+			log.info("Submitted new order {}", json.writeValueAsString(order));
+		} catch (JsonProcessingException e) {
+			log.error("Submitted new order {} but failed to log", e);
+		}
+
 		return ResponseEntity.ok(order);
 	}
 	
--- starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +24,8 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-	
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserRepository userRepository;
 	
@@ -45,18 +48,23 @@ public class UserController {
 	
 	@PostMapping("/create")
 	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		log.debug("Creating User {}", createUserRequest.getUsername());
+
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
 		Cart cart = new Cart();
+
 		cartRepository.save(cart);
 		user.setCart(cart);
 		if(createUserRequest.getPassword() ==null || createUserRequest.getConfirmPassword()==null || createUserRequest.getPassword().length()<7 ||
 				!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())){
-			// TODO Log - request is too short or passwords don't match
+			log.warn("Failed to create user {} - password/confirmPassword not valid", createUserRequest.getUsername());
 			return ResponseEntity.badRequest().build();
 		}
 		user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
 		userRepository.save(user);
+		log.info("Created user {}", createUserRequest.getUsername());
+
 		return ResponseEntity.ok(user);
 	}
 	
